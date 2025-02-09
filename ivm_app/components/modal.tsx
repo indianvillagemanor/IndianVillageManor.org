2f99942c4ce9543d696a155b6a3b3c13bca020ed
Steven Levis <200574+levis501@users.noreply.github.com>
2025-02-09 11:28:32 -0500

    fixed zIndex of modal close button

--- ivm_app/components/modal.tsx
+++ ivm_app/components/modal.tsx
@@ -39,8 +39,8 @@ const Modal: React.FC<ModalProps> = ({ src, onClose }) => {
       className={modalVisibility + " fixed top-0 left-0 w-screen h-screen bg-black/90 justify-center items-center"}
       style={{ zIndex: 80, position: "fixed" }}>
 
-      <a className="fixed z-90 top-6 right-8 text-white text-5xl font-bold"
-        onClick={onClose}>
+      <a className="fixed top-6 right-8 text-white text-5xl font-bold"
+        onClick={onClose} style={{ zIndex: 90 }}>
         &times;
       </a>
       <Image
