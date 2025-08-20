299e35221a082c32da0f2243b33dc507171a0cee
Steven Levis <200574+levis501@users.noreply.github.com>
2025-08-20 09:46:50 -0400

    Revert "fixed authOptions export errors"
    This reverts commit 115085d4524253112bd827a43acd06b446ce9577.

--- ivm_app/app/welcome/page.tsx
+++ ivm_app/app/welcome/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/authOptions";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import React from "react";
 
 export default async function WelcomePage() {
